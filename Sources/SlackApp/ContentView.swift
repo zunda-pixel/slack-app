f99c5cdc0345c43d70c114bc602b3fbb37cc4ef8
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-05-05 01:27:38 +0900

    remove BeatyView

--- Sources/SlackApp/ContentView.swift
+++ Sources/SlackApp/ContentView.swift
@@ -61,88 +61,9 @@ struct ContentView: View {
     .tint(Color.primary)
 }
 
-struct BeatyView: View {
-  var text = "プレゼンテーション"
-  
-  var body: some View {
-    VStack {
-      Rectangle()
-        .fill(Color(.secondarySystemGroupedBackground))
-        .frame(width: 300, height: 100)
-        .overlay {
-          Text(text)
-            .padding(.vertical, 8)
-            .padding(.horizontal, 16)
-            .doubleBorder()
-        }
-        .environment(\.colorScheme, .light)
-      Rectangle()
-        .fill(Color(.secondarySystemGroupedBackground))
-        .frame(width: 300, height: 100)
-        .overlay {
-          Text(text)
-            .padding(.vertical, 8)
-            .padding(.horizontal, 16)
-            .doubleBorder()
-        }
-        .environment(\.colorScheme, .dark)
-    }
-  }
-}
-
 #if canImport(AppKit)
 extension NSColor {
   static let secondarySystemGroupedBackground: NSColor = .systemGray
   static let secondarySystemBackground: NSColor  = .systemGray
 }
 #endif
-
-extension View {
-  func doubleBorder() -> some View {
-    modifier(DoubleBorderModifier())
-  }
-}
-
-struct DoubleBorderModifier: ViewModifier {
-  @Environment(\.colorScheme) var colorScheme
-  
-  var outlineColor: Color {
-    switch colorScheme {
-    case .dark: Color.black
-    case .light: Color.secondary
-    @unknown default:
-      fatalError()
-    }
-  }
-  
-  var inlineColor: Color {
-    switch colorScheme {
-    case .dark: Color.secondary
-    case .light: Color.white
-    @unknown default:
-      fatalError()
-    }
-  }
-
-  func body(content: Content) -> some View {
-    content
-      .background {
-        RoundedRectangle(cornerRadius: 10)
-          .fill(Color(.secondarySystemBackground).opacity(0.9))
-          .shadow(color: Color(.secondarySystemBackground), radius: 4)
-          .overlay {
-            RoundedRectangle(cornerRadius: 10)
-              .stroke(outlineColor, lineWidth: 0.3)
-              .overlay {
-                RoundedRectangle(cornerRadius: 9)
-                  .stroke(inlineColor, lineWidth: 0.3)
-                  .padding(1)
-              }
-          }
-      }
-  }
-}
-
-#Preview {
-  BeatyView()
-}
